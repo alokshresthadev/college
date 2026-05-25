5cfd3b394ac3bb6d693316cf060e13083b38a9ae
alokshresthadev <alokshresthadev@gmail.com>
2026-05-26 03:34:16 +0545

    Python uncompleted calculator
    	new file:   Logic.py
    	new file:   UI.py
    	new file:   __pycache__/Logic.cpython-313.pyc

---
+++ Logic.py
@@ -0,0 +1,146 @@
+class CalculatorLogic:
+    def __init__(self):
+        self.first_num=None
+        self.second_num=None
+        self.math=None
+    
+    def get_first_val_and_operator(self,first_num,operator):# suru ko value liney ani operator store garney just that much
+        self.first_value = float(first_num)
+        self.operation = operator
+
+    def get_second_val_and_calculate(self,second_num):
+        self.second_value = float(second_num)
+        if(self.operation=="add"):
+            return self.first_value+self.second_value
+        if(self.operation=="subtract"):
+            return self.first_value-self.second_value
+        if(self.operation=="multiplication"):
+            return self.first_value*self.second_value
+        if(self.operation=="division"):
+            return self.first_value/self.second_value
+        if(self.operation=="power"):
+            return self.first_value**self.second_value
+        
+    def get_val_and_square(self,first_num):
+        self.first_value = float(first_num)*float(first_num)
+        return self.first_value
+    
+    def get_val_and_square_root(self,first_num):
+        self.calculated_value = float(first_num)**(1/2)  # 5^(1/2)= rootunder(5)
+        return self.calculated_value
+
+
+
+    # def button_click(self,number):
+    #     self.current = self.e.get()
+    #     self.e.delete(0,END)
+    #     self.e.insert(0,str(self.current)+str(number))
+    
+    # def button_del(self):
+
+    #     self.e.delete(len(self.e.get())-1,END)   # This line is copied from Chatgpt Couldnot understand how to skip the index and get the second last value Note;;;;; have to search for another method
+    
+    # def button_clear(self):
+    #     self.e.delete(0,END)
+    
+    # def button_Paranthesis(self):
+    #     return
+    
+    # def button_divide(self):
+    #     self.first_num
+    #     self.math
+    #     self.math="division"
+    #     self.first_num=float(self.e.get())
+    #     self.e.delete(0,END)
+    
+    # def button_multiply(self):
+    #     self.first_num
+    #     self.math
+    #     self.math="multiplication"
+    #     self.first_num=float(self.e.get())
+    #     self.e.delete(0,END)
+    
+    # def button_subtract(self):
+    #     self.first_num
+    #     self.math
+    #     self.math="subtraction"
+    #     self.first_num=float(self.e.get())
+    #     self.e.delete(0,END)
+
+    # def button_sqrt(self):
+    #     self.first_num
+    #     self.math
+    #     self.math="sqrt"
+    #     self.first_num=float(self.e.get())
+    #     self.e.delete(0,END)
+
+    # def button_add(self):
+    #     self.first_num
+    #     self.math
+    #     self.math="addition"
+    #     self.first_num=float(self.e.get())
+    #     self.e.delete(0,END)
+
+    # def button_square(self):
+    #     self.current = self.e.get()
+    #     self.e.delete(0,END)
+    #     self.e.insert(0,float(self.current)*float(self.current))
+    
+    # def button_equals(self):
+    #     self.second_num
+    #     self.second_num=float(self.e.get())
+    #     self.e.delete(0,END)
+
+    #     if(self.math == "addition"):
+    #         self.e.insert(0,self.first_num + self.second_num)
+    #     if(self.math == "subtraction"):
+    #         self.e.insert(0,self.first_num - self.second_num)
+    #     if(self.math == "multiplication"):
+    #         self.e.insert(0,self.first_num * self.second_num)
+    #     if(self.math == "division"):
+    #         self.e.insert(0,self.first_num / self.second_num)
+    
+    # def button_power(self):
+    #     return
+    
+    # def button_dot(self):
+    #     return
+    
+    # def button_pie(self):
+    #     return
+    
+    # def button_exp(self):
+    #     return
+    
+    # def button_ans(self):
+    #     return   
+     
+    # def button_log(self):
+    #     return
+    
+    # def button_ln(self):
+    #     return
+    
+    # def button_sin(self):
+    #     return
+    
+    # def button_cos(self):
+    #     return
+    
+    # def button_tan(self):
+    #     return
+    
+    # def button_python(self):
+    #     return
+    
+    # def button_asm(self):
+    #     return
+    
+    # def button_auto(self):
+    #     return
+    
+    # def button_history(self):
+    #     return
+    
+    # def button_factorial(self):
+    #     return
